840a173d1a058c692fd89d8e5c49f0485ba9209b
PRASHANT KUMAR <prashantkumar75258@gmail.com>
2026-08-16 18:50:56 +0530

    Initial release: Scientific Calculator v1.0

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("===== Scientific Calculator v1.0 =====")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num1 + num2
+
+elif operator == "-":
+    result = num1 - num2
+
+elif operator == "*":
+    result = num1 * num2
+
+elif operator == "/":
+    if num2 == 0:
+        result = "Error: Cannot divide by zero"
+    else:
+        result = num1 / num2
+
+else:
+    result = "Invalid operator"
+
+print("Result:", result)
